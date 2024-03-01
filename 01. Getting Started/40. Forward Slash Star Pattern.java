c90968a975a66f709eea7736b1a3ffedaa376c60
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-03-01 16:22:32 +0530

    Forward Slash Star Pattern

---
+++ 01. Getting Started/40. Forward Slash Star Pattern.java
@@ -0,0 +1,60 @@
+// Forward Slash Star Pattern.
+
+// Sample Input:
+// 6
+    
+// Sample Output:
+//      *
+//     *
+//    *
+//   *
+//  *
+// *
+
+
+import java.util.*;
+public class Main 
+{
+    public static void secondApproach(int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(i + j == n + 1)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void firstApproach(int n)
+    {
+        int nsp = n - 1;
+        for(int row = 1; row <= n; row++)
+        {
+            for(int csp = 1; csp <= nsp; csp++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            nsp--;
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        firstApproach(n);
+        System.out.println();
+        secondApproach(n);
+    }
+}
