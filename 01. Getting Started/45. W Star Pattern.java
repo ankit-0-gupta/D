248bd31d928b3f6181c3c14e5b3193f9a6203d61
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-03-02 12:26:17 +0530

    W Star Pattern

---
+++ 01. Getting Started/45. W Star Pattern.java
@@ -0,0 +1,46 @@
+// W Star Pattern.
+
+// Sample Input: 
+// 5
+
+// Sample Output:
+// *   *
+// *   *
+// * * *
+// ** **
+// *   *
+
+
+import java.util.*;
+public class Main 
+{
+    public static void wPattern(int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= n; j++)
+            {
+                if(j == 1 || j == n)
+                {
+                    System.out.print("*");
+                }
+                else if(i > n / 2 && (i == j || i + j == n + 1))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        wPattern(n);
+    }
+}
