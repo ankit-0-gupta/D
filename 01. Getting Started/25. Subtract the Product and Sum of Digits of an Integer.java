24e5df9d0c3ede1e4050161cc954e985170b7437
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-03-05 15:54:53 +0530

    Subtract the Product and Sum of Digits of an Integer

---
+++ 01. Getting Started/25. Subtract the Product and Sum of Digits of an Integer.java
@@ -0,0 +1,21 @@
+// Subtract the Product and Sum of Digits of an Integer.
+
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+
+class Solution 
+{
+    public int subtractProductAndSum(int n) 
+    {
+        int product = 1;
+        int sum = 0;
+        while(n != 0)
+        {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+        return (product - sum);
+    }
+}
