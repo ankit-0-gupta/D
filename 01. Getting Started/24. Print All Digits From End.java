49f1b855e9efb84f13f775563a76608eb8dac7c9
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-03-05 15:35:39 +0530

    Print All Digits From End

---
+++ 01. Getting Started/24. Print All Digits From End.java
@@ -0,0 +1,45 @@
+// Print All Digits From End.
+
+// Sample Input 0:
+// 987651
+  
+// Sample Output 0:
+// 1
+// 5
+// 6
+// 7
+// 8
+// 9
+
+// Sample Input 1:
+// 10000
+  
+// Sample Output 1:
+// 0
+// 0
+// 0
+// 0
+// 1
+
+
+import java.io.*;
+import java.util.*;
+public class Solution 
+{
+    public static void printDigits(int n)
+    {
+       while(n != 0)
+       {
+           int remainder = n % 10;
+           System.out.println(remainder);
+           n /= 10;
+       }
+    }
+    
+    public static void main(String[] args) 
+    {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        printDigits(n);
+    }
+}
