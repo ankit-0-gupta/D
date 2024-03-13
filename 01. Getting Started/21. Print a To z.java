97f23b4260b70bcdafbacfa2b2b4efdd932566e5
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-03-14 01:18:06 +0530

    Print a To z

---
+++ 01. Getting Started/21. Print a To z.java
@@ -0,0 +1,23 @@
+// Print a to z.
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        for(char ch = 'a'; ch <= 'z'; ch++)
+        {
+            System.out.println(ch);
+        }
+        
+        
+        System.out.println();
+        System.out.println();
+        
+        
+        for(int i = 97; i <= 122; i++)
+        {
+            System.out.println((char)i);
+        }
+    }
+}
