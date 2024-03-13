62a470869d7bb7f00f506f3891863c55fcd7a243
Ankit Gupta <112970191+ankit-0-gupta@users.noreply.github.com>
2024-03-14 01:30:42 +0530

    Break and Continue Keyword

---
+++ 01. Getting Started/17. Break and Continue Keyword.java
@@ -0,0 +1,51 @@
+// Break and Continue Keyword.
+
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+//         for(int i = 1; i <= 10; i++)
+//         {
+//             System.out.println(i);
+//             if(i == 5)
+//             {
+//                 break;
+//             }
+//             System.out.println(i);
+//         }
+//         System.out.println();
+//         System.out.println();
+//         for(int i = 1; i <= 10; i++)
+//         {
+//             System.out.println(i);
+//             if(i == 5)
+//             {
+//                 continue;
+//             }
+//             System.out.println(i);
+//         }
+        
+        
+        // for(int i = 1; i <= 20; i++)
+        // {
+        //     if(i == 10)
+        //         break;
+        //     if(i % 2 == 0)
+        //         continue;
+        //     System.out.println(i);
+        // }
+        
+        
+        // for(int i = 1; i <= 3; i++)
+        // {
+        //     System.out.println(i);
+        //     if(i == 1)
+        //     {
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //     System.out.println(i);
+        // }
+    }
+}
